549bcd99d03b98401344a2ca341df3e773906697
Simon Belialov <ssbelow@mail.ru>
2018-10-17 16:57:39 +0300

    proceed navigation

--- app/src/main/java/com/roix/semenbelalov/sandbox/activities/DetailsActivity.kt
+++ app/src/main/java/com/roix/semenbelalov/sandbox/activities/DetailsActivity.kt
@@ -1,22 +1,18 @@
 package com.roix.semenbelalov.sandbox.activities
 
 
-import androidx.appcompat.widget.Toolbar
-import com.roix.semenbelalov.common.ui.activities.BaseToolbarActivity
+import com.roix.semenbelalov.common.ui.activities.BaseLifecycleActivity
 import com.roix.semenbelalov.featuredetails.ui.viewmodels.DetailsViewModel
 import com.roix.semenbelalov.sandbox.R
-import com.roix.semenbelalov.sandbox.databinding.ActivityDetailsBinding
 
 /**
  * Created by roix template
  * https://github.com/roixa/RoixArchitectureTemplates
  */
 
-class DetailsActivity : BaseToolbarActivity<DetailsViewModel, ActivityDetailsBinding>() {
+class DetailsActivity : BaseLifecycleActivity<DetailsViewModel>() {
 
     override fun getLayoutId(): Int = R.layout.activity_details
-
-    override fun getToolbar(): Toolbar? = getBinding()?.toolbar!!.tb
 
 }
 
